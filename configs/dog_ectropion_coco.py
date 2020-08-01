3944e37f39ffe3b83fbb3a93b1e1a19980da6922
seoilgun <seoilgun@gmail.com>
2020-08-02 03:01:13 +0900

    dog ectropion dataset upload

--- configs/dog_ectropion_coco.py
+++ configs/dog_ectropion_coco.py
@@ -228,7 +228,7 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
-        type='LoadAnnotations', with_bbox=True, with_mask=True, with_seg=True),
+        type='LoadAnnotations', with_bbox=True, with_mask=True, with_seg=False),
     dict(
         type='Resize',
         img_scale=[(1333, 400), (1333, 1200)],
@@ -241,7 +241,7 @@ train_pipeline = [
     dict(type='DefaultFormatBundle'),
     dict(
         type='Collect',
-        keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks', 'gt_semantic_seg']),
+        keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),#, 'gt_semantic_seg']),
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
